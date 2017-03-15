e20441b0b27220abeaedafe5bb9f0772853ac28f
Arturo Guzman <arturo@guzart.com>
2017-03-15 10:59:38 -0400

    feat(token): wire subject claim

--- lib/toktok/token.rb
+++ lib/toktok/token.rb
@@ -1,22 +1,48 @@
 module Toktok
+  InvalidIdentity = Class.new(StandardError)
+
   class Token
     attr_reader :config, :jwt, :payload
 
-    def initialize(subject: nil, jwt: nil)
+    def initialize(identity: nil, jwt: nil)
       @config = Toktok.config
-
+      @payload = {}
       if jwt
-        @jwt = jwt
-        decoded_token = JWT.decode(jwt, nil, false)
-        @payload = decoded_token[0]
-        # @header = decoded_token[1]
-      elsif subject
-        @jwt = JWT.encode({ sub: subject }, config.secret_key, config.algorithm)
+        initialize_decode(jwt, identity)
+      else
+        initialize_encode(identity)
       end
     end
 
-    def subject
-      @payload['sub']
+    def identity
+      payload['sub']
+    end
+
+    private
+
+    def initialize_encode(identity)
+      @payload['sub'] = identity
+      @jwt = JWT.encode(payload, config.secret_key, config.algorithm)
+    end
+
+    def initialize_decode(jwt, identity)
+      @jwt = jwt
+      options = decode_options(identity)
+      decoded_token = JWT.decode(jwt, config.secret_key, algorithm?, options)
+      @payload = decoded_token[0]
+      @header = decoded_token[1]
+    rescue JWT::InvalidSubError
+      raise InvalidIdentity, "Invalid identity. Expected #{identity}"
+    end
+
+    def decode_options(identity)
+      options = { algorithm: config.algorithm }
+      options = options.merge(sub: identity, verify_sub: true) if identity
+      options
+    end
+
+    def algorithm?
+      config.algorithm != 'none'
     end
   end
 end
--- spec/lib/toktok/token_spec.rb
+++ spec/lib/toktok/token_spec.rb
@@ -1,31 +1,65 @@
 describe Toktok::Token do
-  let(:config) do
-    Toktok::Configuration.new(
-      algorithm: 'HS256',
-      secret_key: '5ef620dda4ef4ee26b422c98609ca20a'
-    )
-  end
+  let(:algorithm) { 'HS256' }
+  let(:secret_key) { '5ef620dda4ef4ee26b422c98609ca20a' }
 
-  let(:jwt) do
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEyM30.I3qJQ85nJSF2igrbZAvPFYAtBco_6xz4CKI2bWi3uVI'
-  end
+  def prepare_config(**args)
+    attrs = { algorithm: algorithm, secret_key: secret_key }.merge(args)
+    config = Toktok::Configuration.new(**attrs)
 
-  before(:each) do
     instance_double(Toktok)
     allow(Toktok).to receive(:config).and_return(config)
   end
 
   describe 'encoding' do
-    it 'encodes a jwt token when initialized using a subject' do
-      token = Toktok::Token.new(subject: 123)
-      expect(token.jwt).to eq(jwt)
+    it 'encodes a jwt token when initialized using an identity' do
+      prepare_config
+      token = Toktok::Token.new(identity: 123)
+      expect(token.jwt).to eq(
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEyM30.I3qJQ85nJSF2igrbZAvPFYAtBco_6xz4CKI2bWi3uVI'
+      )
+    end
+
+    it 'uses the identity for the Subject claim' do
+      prepare_config
+      token = Toktok::Token.new(identity: 123)
+      expect(token.payload['sub']).to eq(123)
     end
   end
 
   describe 'decoding' do
-    it 'decodes a jwt token when initialized with a jwt' do
-      token = Toktok::Token.new(jwt: jwt)
-      expect(token.subject).to eq(123)
+    describe 'when initialized with a jwt' do
+      it 'decodes a jwt token' do
+        prepare_config
+        jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEyM30.I3qJQ85nJSF2igrbZAvPFYAtBco_6xz4CKI2bWi3uVI'
+        token = Toktok::Token.new(jwt: jwt)
+        expect(token.identity).to eq(123)
+      end
     end
+
+    describe 'when initialized with a jwt and a subject' do
+      it 'verifies the Subject claim when initialized with a :jwt and a :subject' do
+        prepare_config
+        jwt = JWT.encode({ sub: 345 }, secret_key, algorithm)
+        expect { Toktok::Token.new(jwt: jwt, identity: 123) }.to raise_error(Toktok::InvalidIdentity)
+      end
+    end
+
+    describe 'without an algorithm' do
+      it 'decodes a jwt token' do
+        prepare_config(algorithm: 'none')
+        jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEyM30.'
+        token = Toktok::Token.new(jwt: jwt)
+        expect(token.identity).to eq(123)
+      end
+    end
+
+    # it 'verifies the Subject claim' do
+    #   real_signature = 'I3qJQ85nJSF2igrbZAvPFYAtBco_6xz4CKI2bWi3uVI'
+    #   highjacked_jwt = JWT.encode({ sub: 345 }, secret_key, algorithm)
+    #   # other_header_payload = .split('.').take(2)
+    #   # highjacked_jwt = other_header_payload.concat([real_signature]).join('.')
+    #   # highjacked_jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEyM30.I3qJQ85nJSF2igrbZAvPFYAtBco_6xz4CKI2bWi3uVI'
+    #   token = Toktok::Token.new(jwt: highjacked_jwt)
+    # end
   end
 end
